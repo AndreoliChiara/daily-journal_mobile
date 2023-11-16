6ab17b299369a8e8a35d6b3d0f2d13c89df33e69
AndreoliChiara <chiara.andreoli.2002@gmail.com>
2023-11-16 14:19:53 +0100

    prova accellerometro 2

--- src/app_1.js
+++ src/app_1.js
@@ -1,3 +1,31 @@
+
+if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', handleMotion);
+} else {
+    console.error('L\'accelerometro non è supportato su questo dispositivo.');
+}
+
+function handleMotion(event) {
+    const acceleration = event.accelerationIncludingGravity;
+    const threshold = 9.5; // Puoi regolare questo valore in base alle tue esigenze
+
+    // Verifica se il telefono è in posizione orizzontale
+    if (acceleration.z > threshold) {
+        // Avvia la domanda solo se ci sono ancora domande disponibili
+        if (questionIndex < questions.length) {
+            const question = questions[questionIndex];
+            questionContainer.innerHTML = '';
+            typeQuestion(question, questionContainer);
+            readQuestion(question);
+
+            // Incrementa l'indice della domanda corrente
+            questionIndex++;
+        }
+    }
+}
+
+
+
 //Data corrente
 
 
@@ -17,31 +45,7 @@ var dataFormattata = anno + '-' + (mese < 10 ? '0' + mese : mese) + '-' + (giorn
         document.body.innerHTML += '<p>Data corrente: ' + dataFormattata + '</p>';
 
 
-        // Aggiungi questo codice per gestire l'accelerometro
-    if (window.DeviceMotionEvent) {
-            window.addEventListener('devicemotion', handleMotion);
-    } else {
-             console.error('L\'accelerometro non è supportato su questo dispositivo.');
-    }
-
-    function handleMotion(event) {
-        const acceleration = event.accelerationIncludingGravity;
-        const threshold = 9.5; // Puoi regolare questo valore in base alle tue esigenze
-
-        // Verifica se il telefono è in posizione orizzontale
-        if (acceleration.z > threshold) {
-        // Avvia la domanda solo se ci sono ancora domande disponibili
-            if (questionIndex < questions.length) {
-                const question = questions[questionIndex];
-                questionContainer.innerHTML = '';
-                typeQuestion(question, questionContainer);
-                readQuestion(question);
-
-                // Incrementa l'indice della domanda corrente
-                questionIndex++;
-        }
-    }
-}
+        
 
 
 
